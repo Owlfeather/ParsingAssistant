a7a12132502574cfb29777a80b907effaed29347
Полина Мартынюк <polinka1106@yandex.ru>
2019-09-07 17:34:36 +0300

    Init commit

--- ParsingAssistant/ParsingAssistant.h
+++ ParsingAssistant/ParsingAssistant.h
@@ -2,6 +2,7 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_ParsingAssistant.h"
+#include "ui_MainMenu.h"
 
 class ParsingAssistant : public QMainWindow
 {
@@ -11,5 +12,5 @@ public:
 	ParsingAssistant(QWidget *parent = Q_NULLPTR);
 
 private:
-	Ui::ParsingAssistantClass ui;
+	Ui::MainWindow ui;
 };
